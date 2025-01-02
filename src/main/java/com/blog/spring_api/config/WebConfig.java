ffee47ee24d5ed00dca0c4bfe932c28a5f486571
deepria <eojin823@gmail.com>
2025-01-02 17:33:00 +0900

    [config] front url cors 해제

--- src/main/java/com/blog/spring_api/config/WebConfig.java
+++ src/main/java/com/blog/spring_api/config/WebConfig.java
@@ -28,7 +28,7 @@ public class WebConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**") // 모든 경로에 대해 허용
-                        .allowedOrigins("http://localhost:5173") // Vue 개발 서버 도메인
+                        .allowedOrigins("https://main.d39hqh4ds9p1ue.amplifyapp.com") // Vue 개발 서버 도메인
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // 허용할 HTTP 메서드
                         .allowedHeaders("*") // 모든 헤더 허용
                         .allowCredentials(true); // 인증 정보 허용
