d3402fa8a0c10e1b9142d3b4c3aa8670c91a9ee4
ejlee <eojin823@gmail.com>
2025-04-11 10:03:46 +0900

    [LB/TG] 헬스체크용 api 응답 값 수정

--- src/main/java/com/blog/spring_api/controller/MainController.java
+++ src/main/java/com/blog/spring_api/controller/MainController.java
@@ -1,5 +1,6 @@
 package com.blog.spring_api.controller;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,7 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/")
 public class MainController {
     @GetMapping("/helloWorld")
-    public int healthCheck() {
-        return 1;
+    public HttpStatus healthCheck() {
+        return HttpStatus.OK;
     }
 }
